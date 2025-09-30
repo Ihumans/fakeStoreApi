20d53a9b2902a1c3b35439c86f49f9cf2a51e0e5
theRuneCrew <runeteamofficial@gmail.com>
2025-09-30 23:51:49 +0330

    req to fake store api and get data

--- src/assets/FakeStore.jsx
+++ src/assets/FakeStore.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import axios from "axios";
+import { useCallback, useEffect, useState } from "react";
 
 export default function FakeStore() {
   const [data, setData] = useState([]);
@@ -6,11 +7,27 @@ export default function FakeStore() {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
 
+  const itemsReq = useCallback(async () => {
+    try {
+      setIsLoading(true);
+      const response = await axios.get('https://fakestoreapi.com/products');  
+      setData(response.data);
+      
+    } catch (err) {
+        setError("خطا در گرفتن اطلاعات");
+    } finally {
+        setIsLoading(false);
+    }
+}, []);
+
+useEffect(() => {
+      console.log(data);
+    itemsReq();
+  }, [itemsReq]);
+
   return (
-  <div className="p-6">
-    <h1 className="text-2xl font-bold mb-4">
-        Fakestorapi
-    </h1>
-  </div>
-  )
+    <div className="p-6">
+      <h1 className="text-2xl font-bold mb-4">Fakestorapi</h1>
+    </div>
+  );
 }
